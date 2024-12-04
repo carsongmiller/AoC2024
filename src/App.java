eb24f643233c3ff6a042a21af46fd4dc8961f9a5
carsongmiller <carsongmiller@gmail.com>
2024-12-04 14:07:32 -0600

    Initial commit (no days done yet)

--- src/App.java
+++ src/App.java
@@ -5,9 +5,17 @@ public class App {
     public static void main(String[] args) {
 		String inputFilePath = "src\\01.txt";
 		List<String> lines = GetLines(inputFilePath);
-        System.out.println(Part1(lines));
-		System.out.println(Part2(lines));
+        System.out.println("Part 1: " + Part1(lines));
+		System.out.println("Part 2: " + Part2(lines));
     }
+
+    public static int Part1(List<String> lines) {
+        return 0;
+    }
+
+	public static int Part2(List<String> lines) {
+        return 0;
+	}
 
     public static List<String> GetLines(String filePath) {
         //Create a list that we'll store all of our lines in
@@ -27,32 +35,4 @@ public class App {
 
 		return lines;
     }
-
-    public static int Part1(List<String> lines) {
-        int sum = 0;
-        for (String line : lines) {
-            char left = ' ';
-            char right = ' ';
-            //find the left most digit
-            for (int i  = 0; i < line.length(); i++) {
-                if (left == ' ' && Character.isDigit(line.charAt(i))) {
-                    left = line.charAt(i);
-                }
-            }
-
-            //find the right most digit
-            for (int i  = line.length() - 1; i >= 0; i--) {
-                if (right == ' ' && Character.isDigit(line.charAt(i))) {
-                    right = line.charAt(i);
-                }
-            }
-
-            sum += Integer.parseInt(Character.toString(left) + Character.toString(right));
-        }
-        return sum;
-    }
-
-	public static int Part2(List<String> lines) {
-        return 0;
-	}
 }
